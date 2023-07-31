c59948fe179d319d14f114791a479473696196c8
Dirtybug36 <104801789+Dirtybug36@users.noreply.github.com>
2023-07-31 13:36:38 +0100

    basic outline is finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Bill />
+        <Service />
+
+        <Payment />
+        <Button />
+      </>
     </div>
   );
+}
+function Bill() {
+  return (
+    <div>
+      <p>How much was the bill?</p>
+      <input type="text" />
+    </div>
+  );
+}
+function Service() {
+  return (
+    <>
+      <p>How did you like the services?</p>
+      <select>
+        <option>Dissatisfied(0%)</option>
+        <option>It was ok(5%)</option>
+        <option>It was good(10%)</option>
+        <option>It was amazing(20%)</option>
+      </select>
+      <Friend />
+    </>
+  );
+}
+function Friend() {
+  return (
+    <>
+      <p>How did your friend like the services?</p>
+      <select>
+        <option>Dissatisfied(0%)</option>
+        <option>It was ok(5%)</option>
+        <option>It was good(10%)</option>
+        <option>It was amazing(20%)</option>
+      </select>
+    </>
+  );
+}
+function Payment() {
+  return (
+    <p>
+      You pay ${} (${} + ${}tip)
+    </p>
+  );
+}
+function Button() {
+  return <button>Reset</button>;
 }
 
 export default App;
