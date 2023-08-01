26077f5ba73cafeb64071b2bb9bff9609fcb667e
Dirtybug36 <104801789+Dirtybug36@users.noreply.github.com>
2023-08-01 16:00:09 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -5,25 +5,38 @@ import Payment from "./components/Payment";
 import Service from "./components/Service";
 
 function App() {
-  const [input, setInput] = useState(0);
-  const [tips, setTips] = useState(0);
-  const [friendTips, setfriendTips] = useState(0);
+  const [input, setInput] = useState(0); //for the amount they we enter
+
+  const [tips, setTips] = useState(0); //to calculate our tips
+
+  const [friendTips, setfriendTips] = useState(0); //to calculate friends tip
+
+  //to reset all data to initial value
   function resetHandler() {
     setInput(0);
     setTips(0);
     setfriendTips(0);
   }
+
+  const isAmountValid = input > 0;
+  //render App component
   return (
     <div className="App">
       <>
+        {/* Bill component to enter the amount */}
         <Bill input={input} setInput={setInput} />
+
+        {/* to calculate tips */}
         <Service value={tips} setValue={setTips}>
           How did you like the services?
         </Service>
+
         <Service value={friendTips} setValue={setfriendTips}>
           How did your Friend like the services?
         </Service>
-        {input <= 0 ? null : (
+
+        {/* display the payment and button components */}
+        {isAmountValid && (
           <>
             <Payment money={input} tips={tips} friendTips={friendTips} />
 
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -2,12 +2,10 @@ export default function Payment({ money, tips, friendTips }) {
   //console.log(money);
   const average = ((tips + friendTips) * money) / 2;
   const totalTips = Number(average) + Number(money);
-  //console.log(average);
-  // console.log(totalTips);
+
   return (
     <p>
       You pay ${totalTips} (${money} + ${Math.round(average)}tip)
-      {/* roundOff the tip later */}
     </p>
   );
 }
